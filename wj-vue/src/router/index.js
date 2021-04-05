45458a0a62f5a5fc4fe98f056f69237ffa60d726
FrankScarlet <hferfrank@gmail.com>
2021-04-05 11:34:06 +0800

    add 导航栏，侧边栏

--- wj-vue/src/router/index.js
+++ wj-vue/src/router/index.js
@@ -2,22 +2,41 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import AppIndex from '../components/AppIndex'
 import Login from '../components/Login'
+import Home from '../components/Home'
+import LibraryIndex from '../components/library/LibraryIndex'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/home',
+    name: 'Home',
+    component: Home,
+    // home 页面并不需要被访问
+    redirect: '/index',
+    children :[
+      {
+        path: '/index',
+        name: 'AppIndex',
+        component: AppIndex,
+        meta:{
+          requireAuth: true
+        }
+      },
+      {
+        path: '/library',
+        name: 'Library',
+        component: LibraryIndex,
+        meta: {
+          requireAuth: true
+        }
+      }
+    ]
+  },
+  {
     path: '/login',
     name: 'Login',
     component: Login
-  },
-  {
-    path: '/index',
-    name: 'AppIndex',
-    component: AppIndex,
-    meta:{
-      requireAuth: true
-    }
   }
   // {
   //   path: '/',
