989c54814350feb5d5dab5dd84e97cdb0fc27ab5
FrankScarlet <hferfrank@gmail.com>
2021-04-08 17:44:33 +0800

    新增前台逻辑，修改一个axios response属性问题

--- wj-vue/src/components/library/SideMenu.vue
+++ wj-vue/src/components/library/SideMenu.vue
@@ -36,11 +36,28 @@
 </template>
 
 <script>
-  export default {
-    name: 'SideMenu'
-  }
+    export default {
+      name: 'SideMenu',
+      data () {
+        return {
+          cid: ''
+        }
+      },
+      methods: {
+        handleSelect (key, keyPath) {
+          this.cid = key
+          this.$emit('indexSelect')
+        }
+      }
+    }
 </script>
 
 <style scoped>
-
+  .categories {
+    position: fixed;
+    margin-left: 50%;
+    left: -600px;
+    top: 100px;
+    width: 150px;
+  }
 </style>
